c2ced0cd3dd258bfc03ebd8efeb66b1276ee1f1e
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,9 @@
+# How to enable snap to the middle in the ZoomTrackBarControl 
+
+
+<p>This example demonstrates how to add snap to the middle in the the ZoomTrackBarControl control. </p>
+<p>First, we added two properties in the custom RepositoryItemZoomTrackBar: SnapToMiddleValue that allows you to enable or disable snapping and MiddleDistance that gives you the opportunity to change the distance (as a percent) from the middle of a trackbar when snapping takes effect.</p>
+
+<br/>
+
+
---
+++ VB/ZoomTrackBarControl_MiddleValue/Form1.vb
@@ -0,0 +1,26 @@
+﻿Imports Microsoft.VisualBasic
+Imports System
+Imports System.Collections.Generic
+Imports System.ComponentModel
+Imports System.Data
+Imports System.Drawing
+Imports System.Linq
+Imports System.Text
+Imports System.Windows.Forms
+
+Namespace ZoomTrackBarControl_MiddleValue
+	Partial Public Class Form1
+		Inherits DevExpress.XtraEditors.XtraForm
+		Public Sub New()
+			InitializeComponent()
+		End Sub
+
+		Private Sub Form1_Load(ByVal sender As Object, ByVal e As EventArgs) Handles MyBase.Load
+			Dim table As New DataTable()
+			table.Columns.Add("Column")
+			table.Rows.Add(True)
+			Me.gridControl1.DataSource = table
+		End Sub
+
+	End Class
+End Namespace
